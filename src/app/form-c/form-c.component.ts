0b97ae845873f951b02c782906fded628c65f7c2
aymangithub <ayman.sharkawy609@gmail.com>
2020-05-12 06:24:23 +0200

    resolve syntax errors

--- src/app/form-c/form-c.component.ts
+++ src/app/form-c/form-c.component.ts
@@ -1,75 +1,68 @@
 import { Component, OnInit } from '@angular/core';
 import { from } from 'rxjs';
-import {employees} from './employees';
-import{FlashMessagesService} from 'angular2-flash-messages';
-import {Router} from '@angular/router';
+import { employees } from './employees';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-form-c',
   templateUrl: './form-c.component.html',
   styleUrls: ['./form-c.component.css']
-})  
+})
 export class FormCComponent implements OnInit {
-verifyObject = [];
-  user=
-{
-  name:"",
-  Lname:"",
-  age:0
-}
- employee:employees=
-{
-    $key:"",
-    Firstname:"",
-    secondtname:"",
-    email:"",
-    country:"",
-    city:"",
-    phone:"",
-    salary:0
-};
-disableSalary:boolean=true;
-
-mySubmit({value,valid})
-{
-  if(valid)
-  {
-    this.verifyObject=[value.name,value.age,value.Lname] ;
-    //this.verifyObject.push
-  }
-  else
-  {
-    "this value is invalid";
-  }
-
-}
-
-mySubmition({value,valid}:{value:employees,valid:boolean})
+  verifyObject = [];
+  user =
     {
-      if(!valid)
-      {
-        this.flashMessageService.show('please enter your data',{cssClass:'alert-danger',timeout:6000});
-      }
-        else
-      {
-        console.log(this.employee);
-      }
-    
+      name: '',
+      Lname: '',
+      age: 0
+    };
+  employee: employees =
+    {
+      $key: '',
+      Firstname: '',
+      secondtname: '',
+      email: '',
+      country: '',
+      city: '',
+      phone: '',
+      salary: 0
+    };
+  disableSalary =  true;
+
+  mySubmit({ value, valid }) {
+    if (valid) {
+      this.verifyObject = [value.name, value.age, value.Lname];
+      // this.verifyObject.push
     }
-// mySubmition(n)
-//     {
-      
-//     }
+    else {
+     return 'this value is invalid';
+    }
 
-
-  constructor(public flashMessageService : FlashMessagesService
-    ) {   }
-
-  ngOnInit(): void
-   {
-    
-    
   }
 
-  
+  mySubmition({ value, valid }: { value: employees, valid: boolean }) {
+    if (!valid) {
+      this.flashMessageService.show('please enter your data', { cssClass: 'alert-danger', timeout: 6000 });
+    }
+    else {
+      console.log(this.employee);
+    }
+
+  }
+  // mySubmition(n)
+  //     {
+
+  //     }
+
+
+  constructor(public flashMessageService: FlashMessagesService
+  ) { }
+
+  ngOnInit(): void {
+
+
+  }
+
+
 }
